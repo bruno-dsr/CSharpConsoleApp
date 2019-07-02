3911ef56687fa383516e7d31f2e1ffe04bdc46bd
Bruno Ramalho <bruno.dsr@hotmail.com>
2019-07-02 09:53:02 -0300

    Casses Detalhe 1
    Incluindo exercicio Classes Detalhe 1, abordando modificadores de acesso, encapsulamento, sobrecargas, membros estáticos

---
+++ ClassesDetalhe1/ClassesDetalhe1/ClassesDetalhe1/ContaCorrente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ClassesDetalhe1
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        private double Taxa = 5.00;
+
+        public ContaCorrente(int numero, string nome)
+        {
+            Numero = numero;
+            Titular = nome;
+        }
+
+        public ContaCorrente(int numero, string nome, double saldo)
+        {
+            Numero = numero;
+            Titular = nome;
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Numero + ", Titular: " + Titular + ", Saldo: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + Taxa);
+        }
+
+        public void Operacoes()
+        {
+            Console.WriteLine("[1] Deposito");
+            Console.WriteLine("[2] Saque");
+            Console.WriteLine("[3] Saldo");
+            Console.WriteLine("[4] Dados da conta");
+            Console.WriteLine("[0] Encerrar");
+        }
+    }
+}
---
+++ ClassesDetalhe1/ClassesDetalhe1/ClassesDetalhe1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesDetalhe1
+{
+    class Program
+    {
+        //EXERCICIO CLASSES DETALHADO 1
+        //ABORDA: MEMBROS ESTÁTICOS, CONSTRUTOR, SOBRECARGA, ENCAPSULAMENTO, THIS, PROPERTIES, AUTO PROPERTIES E MODIFICADORES DE ACESSO
+        static void Main(string[] args)
+        {
+            ContaCorrente conta;
+            Console.WriteLine("Abertura de Conta");
+            Console.Write("Número: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Titular: ");
+            string nome = Console.ReadLine();
+            Console.Write("Deseja abrir a conta com um depósito inicial? [S/N]  ");
+            char opt = char.Parse(Console.ReadLine());
+
+            if(opt == 'S' || opt == 's')
+            {
+                Console.Write("Informe o saldo inicial: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaCorrente(numero, nome, saldo);
+            }
+
+            else
+            {
+                conta = new ContaCorrente(numero, nome);
+            }
+
+            Console.WriteLine("\nConta criada com sucesso!");
+            Console.WriteLine("\nSelecione uma opção:");
+            conta.Operacoes();
+            int escolha = int.Parse(Console.ReadLine());
+
+            while(escolha != 0)
+            {
+                switch (escolha)
+                {
+                    case 1:
+                        Console.Write("\nInforme o valor a depositar: ");
+                        double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conta.Deposito(deposito);
+                        Console.WriteLine("Depósito realizado com sucesso.");
+                        break;
+
+                    case 2:
+                        Console.Write("\nInforme o valor a sacar (Taxa de saque: R$ 5.00): ");
+                        double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conta.Saque(saque);
+                        Console.WriteLine("Saque realizado com sucesso.");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nO saldo atual é de: R$ " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+                        break;
+
+                    case 4:
+                        Console.WriteLine(conta.ToString());
+                        break;
+
+                    case 0:
+                        break;
+                }
+
+                if (escolha != 0)
+                {
+                    Console.WriteLine("\nSelecione uma opção:");
+                    conta.Operacoes();
+                    escolha = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
