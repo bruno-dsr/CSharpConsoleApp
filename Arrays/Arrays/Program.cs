8e50f4471f39196f86dd627174a95e68e5519a2d
Bruno Ramalho <bruno.dsr@hotmail.com>
2019-07-02 14:57:14 -0300

    Arrays
    Inclusão de exercícios sobre Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Arrays
+{
+    class Program
+    {
+        // EXERCICIOS ARRAYS
+        static void Main(string[] args)
+        {
+            Quarto[] quartos = new Quarto[10];
+
+            Console.Write("Informe a quantidade de quartos a serem alugados: ");
+            int loop = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("Aluguel #" + (i + 1));
+                Console.Write("Ocupante: ");
+                string ocupante = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto escolhido: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                while (quartos[numero] != null)
+                {
+                    Console.Write("Quarto ocupado. Escolha outro quarto: ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+
+                quartos[numero] = new Quarto(numero, ocupante, email);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for (int j = 0; j < 10; j++)
+            {
+                if (quartos[j] != null)
+                {
+                    Console.WriteLine(quartos[j]);
+                }
+            }
+        }
+    }
+}
---
+++ Arrays/Arrays/Quarto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Arrays
+{
+    class Quarto
+    {
+        public int Numero { get; set; }
+        public string Ocupante { get; set; }
+        public string Email { get; set; }
+
+        public Quarto(int numero, string ocupante, string email)
+        {
+            Numero = numero;
+            Ocupante = ocupante;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Numero + ": " + Ocupante + ", " + Email;
+        }
+    }
+}
