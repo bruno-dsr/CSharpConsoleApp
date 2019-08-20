1cf77bee93d82bf54700c0a7e0d3be332c42275d
Bruno Ramalho <bruno.dsr@hotmail.com>
2019-08-20 16:43:59 -0300

    Exercicio Extension Method

---
+++ ExtensionMethods/ExtensionMethods/Extensions/DateTimeExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtensionMethods.Extensions
+{
+    static class DateTimeExtension
+    {
+        static public string ElapsedTime(this DateTime thisDT)
+        {
+            TimeSpan span = DateTime.Now.Subtract(thisDT);
+            string response = string.Empty;
+
+            if (span.TotalHours < 24.0)
+            {
+                response = span.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " horas";
+            }
+
+            else
+            {
+                response = span.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " dias";
+            }
+
+            return response;
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 5, 20, 13, 36, 42);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
