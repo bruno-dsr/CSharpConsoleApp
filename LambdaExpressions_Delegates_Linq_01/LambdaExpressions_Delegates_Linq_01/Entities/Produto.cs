1a5172b7249110c6b93684585bb69896f56275f8
Bruno Ramalho <bruno.dsr@hotmail.com>
2019-08-20 17:42:51 -0300

    Exercicio Expressoes Lambda, Delegates e Linq - Parte 1

---
+++ LambdaExpressions_Delegates_Linq_01/LambdaExpressions_Delegates_Linq_01/Entities/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressions_Delegates_Linq_01.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Nome}, R$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        //Função estática delegate para comparar produtos por nome
+        public static int CompararPorNome(Produto p1, Produto p2)
+        {
+            return p1.Nome.ToUpper().CompareTo(p2.Nome.ToUpper());
+        }
+
+        //Função estática delegate para comparar produtos por nome
+        public static int CompararPorPreco(Produto p1, Produto p2)
+        {
+            return p1.Preco.CompareTo(p2.Preco);
+        }
+    }
+}
---
+++ LambdaExpressions_Delegates_Linq_01/LambdaExpressions_Delegates_Linq_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using LambdaExpressions_Delegates_Linq_01.Entities;
+
+namespace LambdaExpressions_Delegates_Linq_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Produto> produtos = new List<Produto>();
+            produtos.Add(new Produto("TV", 1200.00));
+            produtos.Add(new Produto("Notebook", 2500.00));
+            produtos.Add(new Produto("Videogame", 1650.00));
+
+            //Usando a função estática definida na classe Produto
+            //produtos.Sort(Produto.CompararPorPreco);
+
+            //Usando uma expressão lambda para comparação, substituindo o uso da função estática da classe
+            //A declaração de tipos é inferida
+            //(p1, p2) => p1.Preco.CompareTo(p2.Preco)
+            produtos.Sort((p1, p2) => p1.Preco.CompareTo(p2.Preco));
+
+            foreach (Produto p in produtos)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
