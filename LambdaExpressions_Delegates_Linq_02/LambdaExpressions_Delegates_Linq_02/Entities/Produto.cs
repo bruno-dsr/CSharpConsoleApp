f8290e3dcbd7072f06a7793ec68c6ac0481add30
Bruno Ramalho <bruno.dsr@hotmail.com>
2019-08-22 10:28:23 -0300

    Exercicio Expressoes Lambda, Delegates e Linq - Parte 2

---
+++ LambdaExpressions_Delegates_Linq_02/LambdaExpressions_Delegates_Linq_02/Entities/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressions_Delegates_Linq_02.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Nome}, R$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ LambdaExpressions_Delegates_Linq_02/LambdaExpressions_Delegates_Linq_02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using LambdaExpressions_Delegates_Linq_02.Entities;
+using System.Collections.Generic;
+
+namespace LambdaExpressions_Delegates_Linq_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+            produtos.Add(new Produto("TV", 1500.00));
+            produtos.Add(new Produto("Notebook", 1800.00));
+            produtos.Add(new Produto("IPod", 460.00));
+            produtos.Add(new Produto("Celular", 980.00));
+            produtos.Add(new Produto("Videogame", 1700.00));
+
+            //Trabalho com PREDICATE e DELEGATES
+            //Possível utilizar como Predicate o resultado de uma função lambda.
+            produtos.RemoveAll(p => p.Preco >= 1000.00);
+
+            //Outra forma possível seria utilizando uma função como parâmetro
+            //produtos.RemoveAll(VerificaValor);
+
+            //Opção utilizando delegates
+            //Deleg deleg = VerificaValor;
+            //produtos.RemoveAll(deleg.Invoke);
+
+            foreach (Produto p in produtos)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        //Função estática usada como predicate
+        public static bool VerificaValor(Produto p)
+        {
+            return p.Preco >= 1000.00;
+        }
+
+        //Delegate usado como predicate
+        delegate bool Deleg(Produto p);
+    }
+}
